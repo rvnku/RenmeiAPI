4a12db002d69eb42e22bc8c480d264620959b023
rvnku <artpr.zxc@gmail.com>
2026-07-13 02:52:05 +0500

    refactor: move redirect_uri env to utils/get_redirect_uri()

--- app/env.py
+++ app/env.py
@@ -6,7 +6,6 @@ dotenv.load_dotenv()
 
 
 class Env(ABC):
-    redirect_uri = os.getenv('REDIRECT_URI', 'http://127.0.0.1:8000/auth/callback')
     database_url = os.getenv('DATABASE_URL', 'sqlite:///./tokens.db')
     if not (client_id := os.getenv('CLIENT_ID', '')):
         raise ValueError('CLIENT_ID must be set in environment')
@@ -15,6 +14,6 @@ class Env(ABC):
     if not (api_key := os.getenv('API_KEY', '')):
         raise ValueError('API_KEY must be set in environment')
     host = os.getenv('HOST', '127.0.0.1')
-    port = int(os.getenv('PORT', 8081))
+    port = int(os.getenv('PORT', 8000))
     ssl_sertfile = os.getenv('SSL_SERTFILE')
     ssl_keyfile = os.getenv('SSL_KEYFILE')
